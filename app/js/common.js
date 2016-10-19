20f1b08aeb0aaeaf734b77b75fdba7e399e7f84d
BRspb <bur.rom.vik@gmail.com>
2016-10-19 21:56:38 +0300

    first

---
+++ app/js/common.js
@@ -0,0 +1,42 @@
+$(function() {
+
+	$('.carousel').owlCarousel({
+			loop:true,
+			margin:10,
+			nav:true,
+			// autoplay:true,
+			// autoplayTimeout:4000,
+			responsive:{
+	        0:{
+	            items:1,            
+							navText: ""
+	        }
+	    }
+	});
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
